fa042fe6919392b930bc591b68639c471f73f65b
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:13:47 +0200

    Add files via upload

---
+++ blank_Spaces_or_not/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace blank_Spaces_or_not
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter an number: ");
+            number = Convert.ToInt32(Console.ReadLine());
+            
+
+            Console.WriteLine(number + " " + number + " " + number +" " + number);
+            Console.WriteLine(number + "" + number + "" + number + "" + number);
+            Console.WriteLine(number + " " + number + " " + number + " " + number);
+            Console.WriteLine(number + "" + number + "" + number + "" + number);
+        }
+    }
+}
